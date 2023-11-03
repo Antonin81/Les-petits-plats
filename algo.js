be76294df90b57b1e8410d3e8f9918ff15ca11b9
Antonin PIDET <Antonin.pidet@gmail.com>
2023-11-03 15:55:56 +0100

    Meilleures explications de l'algorithme

--- algo.js
+++ algo.js
@@ -1,28 +1,48 @@
+//list of recipes resulting of a search without using the filters
 let recipesToFilter = [];
 
-function fromFiltersCreateCardsDOM(recipesForDOM, inputList){
-    let cardsDOM = "";
+function resetOptionLists(){
     ingredientsList = [];
     appliancesList = [];
     ustensilsList = [];
-    
+}
+
+//fills the list of options selectable from the displayed recipes
+function buildOptionLists(recipe){
+    recipe.ingredients.forEach(ingredient=>{
+        if(!ingredientsList.includes(ingredient.ingredient)){
+            ingredientsList.push(ingredient.ingredient);
+        }
+    });
+    recipe.ustensils.forEach(ustensil=>{
+        if(!ustensilsList.includes(ustensil)){
+            ustensilsList.push(ustensil);
+        }
+    });
+    if(!appliancesList.includes(recipe.appliance)){
+        appliancesList.push(recipe.appliance);
+    }
+}
+
+//applies the search of the select input bar on the options list
+function researchInSelects(){
+    inputSelectHandler(ingredientsInput, ingredientsList, ingredientsSelect);
+    inputSelectHandler(appliancesInput, appliancesList, appliancesSelect);
+    inputSelectHandler(ustensilsInput, ustensilsList, ustensilsSelect);
+}
+
+//creates the cards DOM according to the selected filters
+function fromFiltersCreateCardsDOM(recipesForDOM, inputList){
+
+    let cardsDOM = "";
+    resetOptionLists();
     let recipesCount = 0;
 
     recipesForDOM.forEach(recipe => {
         if(tests(recipe, inputList)){
-            recipe.ingredients.forEach(ingredient=>{
-                if(!ingredientsList.includes(ingredient.ingredient)){
-                    ingredientsList.push(ingredient.ingredient);
-                }
-            });
-            recipe.ustensils.forEach(ustensil=>{
-                if(!ustensilsList.includes(ustensil)){
-                    ustensilsList.push(ustensil);
-                }
-            });
-            if(!appliancesList.includes(recipe.appliance)){
-                appliancesList.push(recipe.appliance);
-            }
+
+            buildOptionLists(recipe);
+
             cardsDOM+=createCardDOM(recipe);
             recipesCount++;
         }        
@@ -30,38 +50,24 @@ function fromFiltersCreateCardsDOM(recipesForDOM, inputList){
 
     document.getElementById("recipies").innerHTML=cardsDOM;
     createCountDOM(recipesCount);
+    emptySelects();
     createSelectsDOM();
     createOptionHandlers();
-    inputSelectHandler(ingredientsInput, ingredientsList, ingredientsSelect);
-    inputSelectHandler(appliancesInput, appliancesList, appliancesSelect);
-    inputSelectHandler(ustensilsInput, ustensilsList, ustensilsSelect);
+    researchInSelects();
 }
 
+//creates the cards DOM according to string in the main search bar
 function fromSearchCreateCardsDOM(recipesForDOM, text, inputList){
+
     let cardsDOM = "";
-    ingredientsList = [];
-    appliancesList = [];
-    ustensilsList = [];
+    resetOptionLists();
     recipesToFilter = [];
-    
     let recipesCount = 0;
 
     recipesForDOM.forEach(recipe => {
         if(testText(recipe, text)){
             if(inputList.length == 0 || tests(recipe, inputList)){
-                recipe.ingredients.forEach(ingredient=>{
-                    if(!ingredientsList.includes(ingredient.ingredient)){
-                        ingredientsList.push(ingredient.ingredient);
-                    }
-                });
-                recipe.ustensils.forEach(ustensil=>{
-                    if(!ustensilsList.includes(ustensil)){
-                        ustensilsList.push(ustensil);
-                    }
-                });
-                if(!appliancesList.includes(recipe.appliance)){
-                    appliancesList.push(recipe.appliance);
-                }
+                buildOptionLists(recipe);
                 cardsDOM+=createCardDOM(recipe);
                 recipesCount++;
             }
@@ -71,21 +77,27 @@ function fromSearchCreateCardsDOM(recipesForDOM, text, inputList){
 
     document.getElementById("recipies").innerHTML=cardsDOM;
     createCountDOM(recipesCount);
+    emptySelects();
+    createSelectsDOM();
+    createOptionHandlers();
+    researchInSelects();
 }
 
+//Researches recipes from a list of filters
 function research(inputList){
     emptyRecipes();
-    emptySelects();
     if(recipesToFilter.length==0){
         recipesToFilter = [...recipes];
     }
     fromFiltersCreateCardsDOM(recipesToFilter, inputList);
 }
 
+//empties the recipes section
 function emptyRecipes(){
     document.getElementById("recipies").innerHTML="";
 }
 
+//empties the options lists of the selects' DOM
 function emptySelects(){
     document.querySelectorAll(".option-list").forEach(optionList=>{
         optionList.innerHTML="";
@@ -138,26 +150,22 @@ function testText(recipe, text){
 
 }
 
+//Researches recipes from a string
 function searchFromText(stringLength, text, inputList){
+    //applies a search algorithm only if there are 3 letters minimum
     if (stringLength>=3){
         fromSearchCreateCardsDOM(recipes, text, inputList);
-        emptySelects();
-        createSelectsDOM();
-        createOptionHandlers();
-        inputSelectHandler(ingredientsInput, ingredientsList, ingredientsSelect);
-        inputSelectHandler(appliancesInput, appliancesList, appliancesSelect);
-        inputSelectHandler(ustensilsInput, ustensilsList, ustensilsSelect);
     } else {
+        //activates only if there was already a custom recipes display
         if(recipesToFilter.length<recipes.length){
             recipesToFilter = [...recipes];
+            //displays all the recipes if not any filters are applied, else applies the filters on all recipes
             if(inputList.length == 0){
                 emptySelects();
                 createCardsDOM(recipes);
                 createSelectsDOM();
                 createOptionHandlers();
-                inputSelectHandler(ingredientsInput, ingredientsList, ingredientsSelect);
-                inputSelectHandler(appliancesInput, appliancesList, appliancesSelect);
-                inputSelectHandler(ustensilsInput, ustensilsList, ustensilsSelect);
+                researchInSelects();
             } else {
                 research(inputList);
             }
